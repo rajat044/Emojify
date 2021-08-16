1c6c16218f7054405ef543af180e5975f061ece8
Rajat Kumar <53110995+rajat044@users.noreply.github.com>
2021-08-16 10:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ if image is not None:
         emotion_prediction = emotion_model.predict(cropped_img)
         maxindex = int(np.argmax(emotion_prediction))
         cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-    st.image(cv2.resize(frame,(450,450),interpolation = cv2.INTER_CUBIC))
+    tem = cv2.resize(frame,(450,450),interpolation = cv2.INTER_CUBIC)
+   
+    st.image(cv2.cvtColor(tem, cv2.COLOR_GRAY2BRG))
     emoji = cv2.imread('emojis/' + emotion_dict[maxindex].lower() + '.png')
     
     st.image(cv2.resize(emoji,(450,500),interpolation = cv2.INTER_CUBIC))
